bbf7e74eb8d12c170f15bc092a0c71cb266ac80a
Mark Aduol <ma7614@ic.ac.uk>
2016-11-02 13:16:10 +0000

    Working on Codegen.hs.TODO expression translation; statement translation; program translation; optimisation; mapping to assembly instructions

--- Codegen.hs
+++ Codegen.hs
@@ -1,19 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Codegen where
 
--- TODO: Completely revamp... again.
-
-import Data.Map as Map
+import Data.Map.Strict as Map
+import Data.Functor.Identity
 import Control.Monad.State
 import Syntax
 
 data Instruction
-  = SeqInstr Instruction Instruction
-  | Label Label
-  | Load Ident Operand
-  | Store Atom Ident
+  = Label Label
+  | Load Var Operand
+  | Store Atom Var
   | Jmp Label
-  | Call_I Ident Ident [Ident]
-  | Return_I Ident
+  | Call_I Var Var [Var]  -- Storage variable; function identifier; list of param variables
+  | Return_I Var
   deriving (Show)
 
 data Operand
@@ -24,26 +25,50 @@ data Operand
   deriving (Show)
 
 data Atom
-  = Var Ident
+  = Var Var
   | Int_I Int
   deriving (Show)
 
-data UnOp_I = I
+data UnOp_I = IR_NOT deriving (Show)
+
+data BinOp_I = IR_Add | IR_Mul deriving (Show)
 
 type Label = String
 
+type Name = String
+
+type SymbolTable = Map.Map Ident Var
+
+data Var = Named Int | UnNamed Int deriving (Show)
+
 --------------------------------------------------------------------------------
-----------------------------CODE GENERATOR STATE--------------------------------
+------------------------------- CODEGEN STATE ----------------------------------
+data BasicBlock = BasicBlock Name [Instruction] Instruction deriving (Show)
+
+-- We will perform code generation independently for each function. Therefore,
+-- the scope of 'CodegenState' is only within a given function.
+-- 'blockCount' gives the information necessary to determine the index of
+-- the next block to be added (within the function scope).
 data CodegenState
   = CodegenState
-  { vtable :: SymbolTable
-  , ftable :: SymbolTable
-  , tempVar :: Ident
-  , tempVarCount :: Int
-  , instructions :: Instruction } deriving (Show)
+  { currentBlockName :: Name                    -- Name of current block on which we are working
+  , blocks           :: Map.Map Name BlockState -- Map of names to blocks; only 'CodegenState' knows block names.
+  , blockCount       :: Int                     -- the number of blocks in the function scope
+  , vtable           :: SymbolTable             -- Symbol table for variable identifiers
+  , ftable           :: SymbolTable             -- Symbol table for function identifiers
+  , tempVarCount     :: Int                     -- Count of temporary (i.e: UnNamed) variables used so far
+  } deriving (Show)
 
--- Isomorphism between 'Codegen a' and 'State CodegenState a'
-newtype Codegen a = Codegen {runCodegen :: State CodegenState a} deriving (Show)
+data BlockState
+  = BlockState
+  { index      :: Int               -- index of block within function scope
+  , stack      :: [Instruction]     -- Instuctions in the current block
+  , terminator :: Maybe Instruction -- Final instruction in block, which is used to end the block
+  } deriving (Show)
+
+-- We use the state monad to maintain state throughout code generation
+newtype Codegen a = Codegen {runCodegen :: State CodegenState a}
+  deriving (MonadState CodegenState)
 
 instance Functor Codegen where
   fmap f k = k >>= (pure . f)
@@ -54,12 +79,143 @@ instance Applicative Codegen where
             k >>= (pure . f)
 
 instance Monad Codegen where
-  return   = pure
-  cg >>= f = f . (fst $ \cgst -> runState (runCodegen cg) cgst)
+  return  = pure
+  x >>= k = state $ \s ->
+    let (a, s') = runState (runCodegen x) s
+    in runState (runCodegen (k a)) s'
 
-getVar :: Codegen Ident
+makeBlocks :: CodegenState -> [BasicBlock]
+makeBlocks s = fmap createBlock (Map.toAscList (blocks s))
+
+-- Basic function to create a block from name and block state
+createBlock :: (Name, BlockState) -> BasicBlock
+createBlock (name, (BlockState _ ins trm)) = BasicBlock name ins (createTrm trm)
+  where
+    createTrm :: Maybe Instruction -> Instruction
+    createTrm trm = case trm of
+      Just x  -> x
+      Nothing -> error ("No terminating instruction for block: " ++ (show name))
+
+-- Whenever creating the block, we must supply the index to ensure that it
+-- occupies the right position within the list of blocks in the function scope
+emptyBlock :: Int -> BlockState
+emptyBlock index = BlockState index [] Nothing
+
+emptyCodegenState :: CodegenState
+emptyCodegenState = CodegenState "entryBlock" Map.empty 0 Map.empty Map.empty 0
+
+execCodegen :: Codegen a -> CodegenState
+execCodegen cg = execState (runCodegen cg) emptyCodegenState
+
+evalCodegen :: Codegen a -> a
+evalCodegen cg = evalState (runCodegen cg) emptyCodegenState
+
+getVar :: Codegen Var
 getVar = do
-  var <- gets tempVar
-  count <- tempVarCount
-  modify $ \s -> s {tempVarCount = count + 1}
-  return var
+  var <- gets tempVarCount
+  modify $ \s -> s {tempVarCount = var + 1}
+  return (UnNamed var)
+
+currentBlock :: Codegen BlockState
+currentBlock = do
+  blkName <- gets currentBlockName
+  blks <- gets blocks
+  case Map.lookup blkName blks of
+    Just res -> return res
+    Nothing -> error ("No such block: " ++ (show blkName))
+
+addBlock :: Name -> Codegen Name
+addBlock blkName = do
+  blks <- gets blocks
+  blkCount <- gets blockCount
+  let newBlock = emptyBlock blkCount
+  modify $ \s -> s { blocks = Map.insert blkName newBlock (blocks s)
+                   , blockCount = blkCount + 1 }
+  return blkName
+
+setBlock :: Name -> Codegen Name
+setBlock blkName = do
+  modify $ \s -> s { currentBlockName = blkName }
+  return blkName
+
+modifyBlock :: BlockState -> Codegen ()
+modifyBlock newBlock = do
+  blkName <- gets currentBlockName
+  modify $ \s -> s {blocks = Map.insert blkName newBlock (blocks s)}
+
+terminateInstr :: Instruction -> Codegen Instruction
+terminateInstr trm = do
+  blk <- currentBlock
+  modifyBlock $ blk {terminator = Just trm}
+  return trm
+
+transExprs :: [Expr] -> Codegen [Instruction]
+transExprs [expr] = do
+  var <- getVar
+  transExpr expr var
+transExprs (expr:exprs) = do
+  code1 <- transExprs [expr]
+  code2 <- transExprs exprs
+  return (code1 ++ code2)
+
+transExpr :: Expr -> Var -> Codegen [Instruction]
+transExpr (LInt n) var = do
+  blk <- currentBlock
+  let stk = stack blk
+  let instr = Load var (Atom (Int_I n))
+  modifyBlock $ blk {stack = stk ++ [instr]}
+  return [instr]
+
+transExpr (LBool bool) var = do
+  blk <- currentBlock
+  let stk = stack blk
+  let instr = Load var (Atom (Int_I (transBool bool)))
+  modifyBlock $ blk {stack = stk ++ [instr]}
+  return [instr]
+  where
+    transBool bool = case bool of
+      True  -> 1
+      False -> 0
+
+transExpr (Ident ident) var = do
+  blk <- currentBlock
+  let stk = stack blk
+  valSymtab <- gets vtable
+  let a = fromMaybe (Map.lookup ident valSymtab)
+  let instr = Load var (Atom (Var a))
+  modifyBlock $ blk {stack = stk ++ [instr]}
+  return [instr]
+  where
+    fromMaybe :: Maybe a -> a
+    fromMaybe res = case res of
+      Just x -> x
+      Nothing -> error "Identifier not found in symbol table."
+
+transExpr (UnApp unop ex) var = do
+  var1 <- getVar
+  code1 <- transExpr ex var1
+  op <- transUnOp unop
+  blk <- currentBlock
+  let stk = stack blk
+  let instr = Load var (UnApp_I op (Var var1))
+  modifyBlock $ blk {stack = stk ++ [instr]}
+  return (code1 ++ [instr])
+
+transExpr (BinApp binop ex1 ex2) var = do
+  var1 <- getVar
+  var2 <- getVar
+  code1 <- transExpr ex1 var1
+  code2 <- transExpr ex2 var2
+  op <- transBinOp binop
+  blk <- currentBlock
+  let stk = stack blk
+  let instr = Load var (BinApp_I op (Var var1) (Var var2))
+  modifyBlock $ blk {stack = stk ++ [instr]}
+  return (code1 ++ code2 ++ [instr])
+
+transUnOp :: UnOp -> Codegen UnOp_I
+transUnOp (NOT_) = pure IR_NOT
+
+transBinOp :: BinOp -> Codegen BinOp_I
+transBinOp (Add) = pure IR_Add
+transBinOp (Mul) = pure IR_Mul
